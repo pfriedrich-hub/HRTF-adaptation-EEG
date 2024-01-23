c08b88b7fea674d785ecb9a7521ae4a4f1e3bd84
ellisnwj <elli.sinowjewa@gmail.com>
2024-01-23 12:31:00 +0100

    latest changes

--- experiment/EEG.py
+++ experiment/EEG.py
@@ -1,23 +1,25 @@
 import random
-import math
 import freefield
 import slab
 import numpy
 import time
 import datetime
 date = datetime.datetime.now()
+from random import randint
 from matplotlib import pyplot as plt
 from pathlib import Path
-from analysis.localization_analysis import localization_accuracy
-fs = 48828
-slab.set_default_samplerate(fs)
 
-# EEG experiment (experiment 2)
+samplerate = 48828
+slab.set_default_samplerate(samplerate)
+data_dir = Path.cwd() / 'data'
+
+# initial test
 subject_id = '1'
 condition = 'Ears Free'
-data_dir = Path.cwd() / 'HRTF-adaptation-EEG' / 'Experiments' / 'EEG_without_molds' / subject_id / condition
+subject_dir = data_dir / 'Experiments' / 'EEG' / subject_id / condition
 
 repetitions = 3  # number of repetitions per speaker
+target_speakers = (20, 22, 24, 26)
 
 
 
@@ -53,8 +55,6 @@ def familiarization_test(subject_id, data_dir, condition, repetitions):
         interval.play()
 
     # 5% of trials, tone after the inter-stimulus interval and localization
-    from random import randint
-
     x = [randint(0, 239) for p in range(0, 239)]
     if x <= 0.05:
         probe.play()
@@ -69,17 +69,4 @@ def familiarization_test(subject_id, data_dir, condition, repetitions):
     while Path.exists(data_dir / file_name):
         file_name = 'localization_' + subject_id + '_' + condition + date.strftime('_%d.%m') + '_' + str(counter)
         counter += 1
-    played_bell = False
-    print('Starting...')
-    for index in trial_sequence:
-        progress = int(trial_sequence.this_n / trial_sequence.n_trials * 100)
-        if progress == 50 and played_bell is False:
-            freefield.set_signal_and_speaker(signal=bell, speaker=23)
-            freefield.play()
-            freefield.wait_to_finish_playing()
-            played_bell = True
-        trial_sequence.add_response(play_trial(sequence[index], progress))
-        trial_sequence.save_pickle(data_dir / file_name, clobber=True)
-    # freefield.halt()
-    print('localization ole_test completed!')
-    return trial_sequence
+
